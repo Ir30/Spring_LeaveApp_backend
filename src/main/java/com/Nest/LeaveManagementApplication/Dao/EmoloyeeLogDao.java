759db01b9d69d644445a36a1f194c38fb52f9465
irshad007 <irshadmuhammedch8@gmail.com>
2022-12-12 12:38:27 +0530

    dao created

---
+++ src/main/java/com/Nest/LeaveManagementApplication/Dao/EmoloyeeLogDao.java
@@ -0,0 +1,7 @@
+package com.Nest.LeaveManagementApplication.Dao;
+
+import com.Nest.LeaveManagementApplication.Model.EmployeeLog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmoloyeeLogDao extends CrudRepository<EmployeeLog,Integer> {
+}
